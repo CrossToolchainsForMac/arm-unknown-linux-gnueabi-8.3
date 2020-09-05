8d45db74905c76ec448f5e5d27e83672c5b0ffed
Zhang Hao <zhanghao@rock-chips.com>
2020-09-05 22:18:19 +0800

    init repo

---
+++ lib/gcc/arm-unknown-linux-gnueabi/8.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Volumes/AOSP/arm-none-linux-eabi/.build/arm-unknown-linux-gnueabi/src/gcc/configure --build=x86_64-build_apple-darwin19.6.0 --host=x86_64-build_apple-darwin19.6.0 --target=arm-unknown-linux-gnueabi --prefix=/Volumes/AOSP/x-tools/arm-unknown-linux-gnueabi --with-sysroot=/Volumes/AOSP/x-tools/arm-unknown-linux-gnueabi/arm-unknown-linux-gnueabi/sysroot --enable-languages=c,c++ --with-float=soft --with-pkgversion='crosstool-NG 1.24.0' --disable-sjlj-exceptions --enable-__cxa_atexit --disable-libmudflap --disable-libgomp --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --disable-libmpx --with-gmp=/Volumes/AOSP/arm-none-linux-eabi/.build/arm-unknown-linux-gnueabi/buildtools --with-mpfr=/Volumes/AOSP/arm-none-linux-eabi/.build/arm-unknown-linux-gnueabi/buildtools --with-mpc=/Volumes/AOSP/arm-none-linux-eabi/.build/arm-unknown-linux-gnueabi/buildtools --with-isl=/Volumes/AOSP/arm-none-linux-eabi/.build/arm-unknown-linux-gnueabi/buildtools --enable-lto --enable-threads=posix --enable-target-optspace --enable-plugin --enable-gold --disable-nls --disable-multilib --with-local-prefix=/Volumes/AOSP/x-tools/arm-unknown-linux-gnueabi/arm-unknown-linux-gnueabi/sysroot --enable-long-long";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm10tdmi" }, { "float", "soft" }, { "tls", "gnu" } };
